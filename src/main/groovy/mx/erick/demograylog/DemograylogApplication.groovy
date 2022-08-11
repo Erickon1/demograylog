f31b0fdaece5332462e3fbeb12fe4e0aa688b6c1
Erick Roberto Mercado Arroyo <erick@eliiot.com>
2022-08-11 17:58:32 -0500

    first commit

---
+++ src/main/groovy/mx/erick/demograylog/DemograylogApplication.groovy
@@ -0,0 +1,13 @@
+package mx.erick.demograylog
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemograylogApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemograylogApplication, args)
+	}
+
+}
---
+++ src/test/groovy/mx/erick/demograylog/DemograylogApplicationTests.groovy
@@ -0,0 +1,13 @@
+package mx.erick.demograylog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemograylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
